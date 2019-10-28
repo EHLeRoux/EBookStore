16cafff03acd3c8d914173357b5ba310751e43c6
Eduard Le Roux <36410503+Edd321@users.noreply.github.com>
2019-10-28 16:25:00 +0200

    Add files via upload

---
+++ LibraryBackEnd.java
@@ -0,0 +1,151 @@
+package Easy;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.DefaultListModel;
+
+public class LibraryBackEnd {
+	
+	static String password = "12345";
+	static String userName = "myuser";
+	
+	
+	/**
+	 * Method that establishes a connection with the database and creates a database and table if one does not already exists
+	 * 
+	 */
+	public static void database () { 
+		
+	}
+	
+	/**
+	 * 
+	 * Method that shows all the entries in the database currently
+	 * 
+	 * @throws SQLException
+	 */
+	public static DefaultListModel <String> viewAll () throws SQLException { 
+		Connection database = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_db?useSSL=false", userName, password);
+		Statement stateMent = database.createStatement();
+		DefaultListModel <String> listOfBooks = new DefaultListModel <String> ();
+		String sqlSelect = "select * from books";
+		System.out.println("The SQL query is: " + sqlSelect);
+		ResultSet rset = stateMent.executeQuery(sqlSelect);
+		
+		while (rset.next()) {
+			
+			String str =  rset.getInt("id") + ", " + rset.getString("author") + ", " + rset.getString("title")
+			+ ", " + rset.getInt("qty");
+			listOfBooks.addElement(str);
+			
+			
+		}
+		
+		return listOfBooks;
+		
+	}
+	/**
+	 * Method that adds a new book to the database
+	 * @throws SQLException
+	 */
+	public static void add (int id, String author, String title, int qty ) throws SQLException { 
+		
+		Connection database = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_db?useSSL=false", userName, password);
+		Statement stateMent = database.createStatement();
+		PreparedStatement prepareStatement = database.prepareStatement("insert into books values (?,?,?,?)");
+		prepareStatement.setInt(1,id); 
+		prepareStatement.setString(2,author);
+		prepareStatement.setString(3,title);
+		prepareStatement.setInt(4, qty);
+		
+		prepareStatement.executeUpdate();
+		
+		
+		System.out.println(prepareStatement + " records inserted.\n");
+		
+		
+	}
+	
+	/**
+	 * 
+	 * Method that updates any existing books entered into the library
+	 * @throws SQLException
+	 */
+	
+	public static void update (int id, String author, String title, int qty, int id2) throws SQLException { 
+		Connection database = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_db?useSSL=false", userName, password);
+		Statement stateMent = database.createStatement();
+		PreparedStatement prepareStatement = database.prepareStatement("update books set id = ?, author = ?, title = ?, qty = ? where id = ?");
+		prepareStatement.setInt(1,id); 
+		prepareStatement.setString(2,author);
+		prepareStatement.setString(3,title);
+		prepareStatement.setInt(4, qty);
+		prepareStatement.setInt(5, id2);
+		
+		prepareStatement.executeUpdate();
+		
+	}
+	
+	/**
+	 * 
+	 * This method deletes an entry, the user simple clicks on the book they want to delete and the function will
+	 * remove that item from the database. Duplicates will also be removed.
+	 * 
+	 * @throws SQLException
+	 */
+	public static void delete (int id, String author, String title, int qty ) throws SQLException { 
+		
+		
+		Connection database = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_db?useSSL=false", userName, password);
+		Statement stateMent = database.createStatement();
+		PreparedStatement prepareStatement = database.prepareStatement("delete from books where id = ? and author = ? and title = ? and qty = ? ");
+		prepareStatement.setInt(1,id); 
+		prepareStatement.setString(2,author);
+		prepareStatement.setString(3,title);
+		prepareStatement.setInt(4, qty);
+		prepareStatement.executeUpdate();
+	}
+	
+	
+	/**
+	 * 
+	 * This Method searches for a book in the database
+	 * 
+	 * @throws SQLException
+	 */
+	
+	public static DefaultListModel <String> search (int id, String author, String title, int qty ) throws SQLException { 
+		
+		Connection database = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_db?useSSL=false", userName, password);
+		Statement stateMent = database.createStatement();
+		DefaultListModel <String> listOfBooks = new DefaultListModel <String> ();
+		PreparedStatement prepareStatement = database.prepareStatement("select * from books where id = ? or author = ? or title = ? or qty = ? ");
+		System.out.println("Selecting from table...");
+		prepareStatement.setInt(1,id); 
+		prepareStatement.setString(2, author);
+		prepareStatement.setString(3,  title);
+		prepareStatement.setInt(4, qty);
+		ResultSet resultSet = prepareStatement.executeQuery();
+		
+		while (resultSet.next()) { 
+			
+			String str =  resultSet.getInt("id") + ", " + resultSet.getString("author") + ", " + resultSet.getString("title")
+			+ ", " + resultSet.getInt("qty");
+			listOfBooks.addElement(str);
+			
+		}
+		System.out.println("Selected...");
+		return listOfBooks;
+	
+		
+		
+			
+		
+	}
+
+}
---
+++ please_read_me.txt
@@ -0,0 +1,24 @@
+
+Welcome to the Electronic bookstore! 
+
+1. Please don't use commas when searching or adding books to the database. 
+
+2. Clear Button: This button clears the current information entered into the the four fields (id, title, quantity and author) found at the top of the screen. 
+
+3. View Book: You have to select a book in the database and then click on View Book this will then populate the four
+              fields with the selected books information.
+
+4. View All: This button will display all the books in the table / database. 
+
+5. Add: After you enter the information in the four fields you can click add, which will add the book information into the table. 
+
+6. Update: For this button to work you have to have selected a book from the database and click on view book to populate the four fields. 
+           After the four fields have been populated you can edit some of the inofmration and then you click on the update button which will
+           update the current book with the new information enter. 
+
+7. Delete: After you have selected a book you can click on the delete button which will then delete the current book that is selected and remove it from the database. 
+
+8. Search: In order to search put in the information of the book into the fields, if you know only one field, for example the author, enter the author and fill in the other fields
+           with anything, the id with integers, the title with a string and the quantity with an int. The program needs the four fields filled to search through the database. 
+
+For the database connection to work on your computer, make sure you change the username and the password to your localhost on your pc. 
